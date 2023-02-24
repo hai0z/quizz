761d6808d5358cf51a9de655474387eec4a625ce
hai0z <hainguyen56211@gmail.com>
2023-02-24 21:19:04 +0700

    login fix

--- src/page/ViewExamResult/index.js
+++ src/page/ViewExamResult/index.js
@@ -1,6 +1,5 @@
 import { doc, getDoc } from "firebase/firestore";
 import React, { useEffect, useState } from "react";
-import Drawer from "../../components/Drawer/UserDrawer";
 import { db } from "../../firebase";
 import { Link, useParams } from "react-router-dom";
 import { useSelector } from "react-redux";
@@ -24,34 +23,32 @@ function ExamResult() {
     }, [id, user.uid]);
     return (
         <div>
-           
-                <div className="container p-8">
-                    <div className="overflow-x-auto">
-                        <table className="table w-full">
-                            <thead className="text-center">
-                                <tr>
-                                    <th>Tên bài thi</th>
-                                    <th>Số câu hỏi</th>
-                                    <th>Số câu trả lời đúng</th>
-                                    <th>Điểm</th>
-                                    <th></th>
-                                </tr>
-                            </thead>
-                            <tbody className="text-center">
-                                <tr>
-                                    <th>Exam1</th>
-                                    <td>{examRs?.questions.length}</td>
-                                    <td>{examRs?.correctAnswer}</td>
-                                    <td>{examRs?.score}</td>
-                                    <td>
-                                        <Link to={`/descriptions/${id}`}>Xem chi tiết</Link>
-                                    </td>
-                                </tr>
-                            </tbody>
-                        </table>
-                    </div>
+            <div className="container p-8">
+                <div className="overflow-x-auto">
+                    <table className="table w-full">
+                        <thead className="text-center">
+                            <tr>
+                                <th>Tên bài thi</th>
+                                <th>Số câu hỏi</th>
+                                <th>Số câu trả lời đúng</th>
+                                <th>Điểm</th>
+                                <th></th>
+                            </tr>
+                        </thead>
+                        <tbody className="text-center">
+                            <tr>
+                                <th>{examRs?.examName}</th>
+                                <td>{examRs?.questions.length}</td>
+                                <td>{examRs?.correctAnswer}</td>
+                                <td>{examRs?.score}</td>
+                                <td>
+                                    <Link to={`/descriptions/${id}`}>Xem chi tiết</Link>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
                 </div>
-           
+            </div>
         </div>
     );
 }
