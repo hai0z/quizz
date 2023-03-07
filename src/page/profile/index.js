fd915a9d78767e1b558ec6031f26a67bf5a239b1
hai0z <hainguyen56211@gmail.com>
2023-03-07 20:51:51 +0700

    ok

--- src/page/profile/index.js
+++ src/page/profile/index.js
@@ -1,17 +1,58 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import { useAppContext } from "../../context/AppProvider";
 import { useAuthContext } from "../../context/AuthProvider";
 import { BsCoin } from "react-icons/bs";
+import { db } from "../../firebase";
+import { query, collection, getDocs } from "firebase/firestore";
+
+const GetCardModal = ({ open, close, card }) => {
+    return (
+        <div>
+            <input type="checkbox" id="my-modal" className="modal-toggle" />
+            <div className={`modal ${open && "modal-open"}`}>
+                <div className="modal-box">
+                    <h3 className="font-bold text-lg">Get Card</h3>
+                    <p className="py-4">seri: {card.seri}</p>
+                    <p className="py-4">code: {card.code}</p>
+                    <div className="modal-action">
+                        <label
+                            htmlFor="my-modal"
+                            className="btn"
+                            onClick={close}
+                        >
+                            Đóng
+                        </label>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
 function Profile() {
     const user = useSelector((state) => state.authSlice.user);
     const { setTitle } = useAppContext();
     const { handleLogout } = useAuthContext();
+    const [card, setCard] = useState({
+        seri: "",
+        code: "",
+    });
+    const [openModal, setOpenModal] = useState(false);
     useEffect(() => {
         setTitle("Thông tin cá nhân");
     });
 
+    const getCard = async () => {
+        const q = query(collection(db, "card"));
+        const snapdata = await getDocs(q);
+        console.log(snapdata.docs[0].data());
+        setCard({
+            seri: snapdata.docs[0].data().seri,
+            code: snapdata.docs[0].data().code,
+        });
+        setOpenModal(true);
+    };
     return (
         <div className="container min-h-screen flex flex-col">
             <div className="flex items-center justify-center mt-4 flex-col">
@@ -47,9 +88,21 @@ function Profile() {
                             Go to admin page
                         </Link>
                     )}
-                    <Link className="mt-10 btn btn-primary mr-3" to="/addcoin">
-                        Nạp coin
-                    </Link>
+                    {user.role !== "ADMIN" && (
+                        <Link
+                            className="mt-10 btn btn-primary mr-3"
+                            to="/addcoin"
+                        >
+                            Nạp coin
+                        </Link>
+                    )}
+                    <label
+                        htmlFor="my-modal"
+                        onClick={getCard}
+                        className="mt-10 btn btn-primary mr-3"
+                    >
+                        Get Card
+                    </label>
                     <button
                         className="mt-10 btn btn-primary"
                         onClick={handleLogout}
@@ -58,6 +111,11 @@ function Profile() {
                     </button>
                 </div>
             </div>
+            <GetCardModal
+                open={openModal}
+                close={() => setOpenModal(false)}
+                card={card}
+            />
         </div>
     );
 }
